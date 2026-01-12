0441639a29f91a70b157ad62b04614bb3c84eaa8
RC Prasad <rcprasad.2000@gmail.com>
2026-01-12 22:23:05 +0530

    node.js

---
+++ node.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+let todos = [];
+let nextId = 1;
+
+//start the server 
+
+app.listen(PORT , ()=>{
+    console.log(`Server is running on the http://localhost:${PORT}`);
+
+})
+
+//get ALL the todos
+
+app.get('/todos' , (req,res)=>{
+    res.json({
+        sucess : true , 
+        data  : todos
+    });
+});
+
+//get todo specific for an ID 
+
+
+app.get('/todos' , (req,res)=>{
+    const id = parseInt(req.params.id);
+    const todo = todos.find(t=>t.id === id);
+
+    
+    if(!todo){
+        return res.status(404).json({
+            success : false ,
+            message : "Todo not found"
+        });
+    }
+    res.json({
+        success : true ,
+        data    : todo
+    });
+
+});
+
+
+
+
+
+
+
+
